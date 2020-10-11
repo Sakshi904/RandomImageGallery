ae7bbe81fbac1dc487616c2c5f695d3e01f20c2e
Sakshi904 <sakshi.904@gmail.com>
2020-10-11 13:36:19 -0700

    Css changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import SearchBar from './SearchBar'
-import ImageList from './ImageList';
-import unsplash from '../apis/unsplash';
+import React from "react";
+import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
+import unsplash from "../apis/unsplash";
+import "./ImageList.css";
 class App extends React.Component {
-    state = { images: [] };
-    onSearchSubmit = async (term) => {
-        const response = await unsplash.get('/search/photos', {
-            params: { query: term },
-        });
-        this.setState({ images: response.data.results });
-    }
-    render() {
-        return (<div className="ui container" style={{ marginTop: '10px' }}>
-            <SearchBar onSubmit={this.onSearchSubmit} />
-           
-            <ImageList images={this.state.images}/>
-        </div>)
-    }
+  state = { images: [] };
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term },
+    });
+    this.setState({ images: response.data.results });
+  };
+  render() {
+    return (
+      <div>
+        <h2>Image Gallery</h2>
+        <div className="ui container" style={{ marginTop: "10px" }}>
+          <SearchBar onSubmit={this.onSearchSubmit} />
+          <ImageList images={this.state.images} />
+        </div>
+      </div>
+    );
+  }
 }
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React from "react";
 class SearchBar extends React.Component {
-    onFormSubmit = (event) => {
-        event.preventDefault();
-        //console.log(this.state);
-        this.props.onSubmit(this.state);
-    }
-    state = { term: "" };
-    render() {
-        return (<div className="ui segment">
-            <form className="ui form" onSubmit={this.onFormSubmit}>
-                <div className="field">
-                    <label>Image Search</label>
-                    <input type="text" value={this.state.term} 
-                    onChange={e => this.setState({ term: e.target.value })}>
-
-                    </input>
-                </div>
-            </form>
-            </div>)
-    }
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    //console.log(this.state);
+    this.props.onSubmit(this.state);
+  };
+  state = { term: "" };
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <input
+              type="text"
+              value={this.state.term}
+              placeholder="Search Images"
+              onChange={(e) => this.setState({ term: e.target.value })}
+            ></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 export default SearchBar;
